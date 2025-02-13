3c5bea3728fca986af3bb16be151c01a327b273a
BryanWV <wilchesbryan@gmail.com>
2025-02-13 18:27:42 -0500

    Making changes to Program cs

--- STM32IoT/Program.cs
+++ STM32IoT/Program.cs
@@ -1,1 +1,54 @@
-git config
+/* Main program to read serial data from the microcontroller */
+using Microsoft.AspNetCore.SignalR;
+using System.IO.Ports;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapHub<SensorHub>("/sensorHub");
+
+var serialPort = new SerialPort("dev/ttyACM0",115200);
+if (!serialPort.IsOpen)
+{
+	serialPort.Open();
+}
+
+Task.Run(async () =>
+{
+    while (true)
+    {
+        try
+        {
+            string data = serialPort.ReadLine();
+
+            Console.WriteLine($"Received: {data}");
+
+            await app.Services.GetRequiredService<IHubContext<SensorHub>>().Clients.All.SendAsync("ReceiveData", data);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+});
+
+app.Run();
+
+public class SensorHub : Hub { }
+
